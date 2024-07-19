7663975ffe58cbd9e141bebd55e8cf45f0aa12c7
EmmaTheDev01 <worldoffictionrw@gmail.com>
2024-07-19 12:08:49 +0300

    Fixing file naming

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import assignmentRoute from "./routes/assignmentRoutes.js"; // Import assignment
 import groupRoute from "./routes/groupRoutes.js"; // Import group routes
 import postRoute from "./routes/post.js"; // Import post routes
 import userRoute from "./routes/users.js"; // Import user routes
+import conversationRoute from "./routes/conversation.js";
 import { errorHandler } from "./errorHandler.js";
 
 // Load environment variables
@@ -61,6 +62,7 @@ app.use("/api/v1/assignment", assignmentRoute); // Assign assignment routes to /
 app.use("/api/v1/group", groupRoute); // Assign group routes to /api/v1/group
 app.use("/api/v1/post", postRoute); // Assign post routes to /api/v1/post
 app.use("/api/v1/user", userRoute); // Assign user routes to /api/v1/user
+app.use("/api/v1/chat", conversationRoute);
 
 // Start server
 app.listen(port, () => {
